ddee8dc34d3eb7c9b9d42ff82bce639a96e2e447
mtovtin <49452703+mtovtin@users.noreply.github.com>
2021-07-04 15:03:32 +0300

    Add files via upload

---
+++ ThreeDimensionalPoint.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using PointLibrary;
+
+namespace ConsoleTestApp
+{
+    class ThreeDimensionalPoint: Point
+    {
+        public double Z { get; set; }
+        public ThreeDimensionalPoint(double X, double Y,double Z) :base(X,Y)
+        {
+            this.Z = Z;
+        }
+        public override string ToString() => $"Three Dimensional Point :({X};{Y};{Z})";
+        public override double Product() => base.Product() * Z;
+         
+        
+
+    }
+}
